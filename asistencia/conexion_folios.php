869ee1903969f8876756cd58053b8c9feccc3cd8
Ruben Polo Carbajo <polouters@gmail.com>
2016-09-28 12:50:10 +0200

    dfn

--- asistencia/conexion_folios.php
+++ asistencia/conexion_folios.php
@@ -3,9 +3,9 @@ $conexion=mysql_connect("localhost","root","Juf13KpD");
 $cosa=mysql_select_db("controldepersonal");
  
 //Requerimiento para que funcione mysqli_query($link, $query);
-$link = mysqli_connect('localhost', 'root', "root", 'controldepersonal');
+$link = mysqli_connect('localhost', 'root', "Juf13KpD", 'controldepersonal');
 mysqli_set_charset($link, "utf8");
  
-$mysqli = new mysqli('localhost', 'root', "root", 'controldepersonal');
+$mysqli = new mysqli('localhost', 'root', "Juf13KpD", 'controldepersonal');
  
 ?> 
